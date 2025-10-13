af210c0d857ff47a2413c14acba473dcd2b8e27d
FakeDev-404 <ufakedev404@gmail.com>
2025-10-13 22:01:19 +0700

    Page-Error500 finished

--- src/pages/PageError404.jsx
+++ src/pages/PageError404.jsx
@@ -9,7 +9,7 @@ return (
 <div className="body404">
       <div className="container-404">
         <h1>404</h1>
-        <h2>Page not found</h2>
+        <h2>PAGE IS NOT FOUND</h2>
         <p>Something went wrong please try again?</p>
     </div>
 </div>
---
+++ src/pages/PageError500.jsx
@@ -0,0 +1,22 @@
+// src/pages/LoginPage.jsx
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Icon } from '@iconify/react';
+import './Page500.css';
+function Page500({ onAuthAction }) {
+
+return (
+<div className="body500">
+      <div className="container-500">
+        <h1>500</h1>
+        <h2>INTERNAL SERVER ERROR</h2>
+        <p>Something went wrong please try again.</p>
+    </div>
+</div>
+  );
+  };
+
+  
+
+
+export default Page500;
